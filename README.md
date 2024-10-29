824cd4754d8ac5bd74ce54121e2aed8cf69448c0
mose <mose.arcaro@gmail.com>
2024-10-29 11:42:14 +0100

    client-server comunication

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NetworkProject
 ### dependences
-Flask, networkx (pip install)
+Flask, networkx (pip install) <br>
 
 #### comandi mininet
 sudo -E python3 main.py <br>
@@ -8,9 +8,13 @@ mininet> deploy <nome servizio> <path_locale> <host opzionale> -> fa il deploy, 
 mininet> service_count -> lista servizi sugli host <br>
 mininet> stop <nome servizio> <host opzionale> -> stoppa il servzio nell'host, se non specifico l'host rimuove il servizio da ovunque <br>
 mininet> check_status <host> <porta> -> vede la risposta del servizio nell'host <br>
+mininet> initflow <hostX> <hostY> -> crea un flow tra hostX e hostY <br>
+mininet> run_client <hostX> <hostY> -> scambia messaggi tra hostX e hostY <br>
 
 ##### esempio
 sudo -E python3 main.py <br>
+ryu-manager FlowController.py <br> <br>
+
 deploy MyWebApp.py /home/vagrant/comnetsemu/network_project/MyWebApp.py h5 <br>
 service_count <br>
 deploy simpleWS.py /home/vagrant/comnetsemu/network_project/simpleWS.py <br>
@@ -22,5 +26,13 @@ check_status h1 90 <br>
 stop h5 MyWebApp.py <br>
 service_count <br>
 stop simpleWS.py <br>
-service_count <br>
+service_count <br> <br>
+
+deploy server.py /home/vagrant/comnetsemu/network_project/server.py h7 <br>
+deploy client.py /home/vagrant/comnetsemu/network_project/client.py h4 <br>
+run_client h2 h1 9090 <br>
+initflow h2 h1 <br>
+run_client h2 h1 9090 <br>
+
+
 
--- network_project/CustomCLI.py
+++ network_project/CustomCLI.py
@@ -2,12 +2,8 @@ from mininet.cli import CLI
 from mininet.net import Mininet
 from connectivity import FlowManager
 from deployer import WebServiceDeployer
+import socket
 
-#sudo -E python3 main.py
-#mininet> deploy <nome> <path_locale> <host opzionale> -> fa il deploy, se non specifico host seceglie quello con meno servizi
-#service_count -> lista servizi sugli host
-#stop <host opzionale> <nome> -> soppa il servzio nell'host, se non specifico l'host rimuove il servizio da ovunque
-#check_status <host> <porta> -> vede la risposta del servizio nell'host'
 
 class MyCLI(CLI):
     def __init__(self, net, deployer, *args, **kwargs):
@@ -103,5 +99,48 @@ class MyCLI(CLI):
         service_count = self.deployer.get_service_count()
         for host, data in service_count.items():
             print(f"{host}: {data['count']} servizi attivi - {data['services']}")
+
+    def do_run_client(self, line):
+        """
+        Run a client to connect to a server on a specified host and port.
+        Usage: run_client <client_host> <server_host> <port>
+        """
+        args = line.split()
+        if len(args) != 3:
+            print("Usage: run_client <client_host> <server_host> <port>")
+            return
+
+        client_host_name, server_host_name, port = args
+        port = int(port)
+
+        try:
+            client_host = self.mn.get(client_host_name)
+            server_host = self.mn.get(server_host_name)
+            server_ip = server_host.IP()
+        except KeyError as e:
+            print(f"Errore: {e}. Assicurarsi che i nomi degli host siano corretti.")
+            return
+
+        print(f"DEBUG: Connecting from {client_host_name} to {server_host_name} ({server_ip}) on port {port}")
+
+        # Definisci uno script Python inline per eseguire il client
+        client_script = f"""
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+s.connect(('{server_ip}', {port}))
+for i in range(3):
+    msg = f'Messaggio {{i+1}} dal client'
+    s.send(msg.encode())
+    print('Inviato al server:', msg)
+    response = s.recv(1024)
+    print('Risposta dal server:', response.decode())
+s.close()
+"""
+
+        # Esegui lo script sul client
+        output = client_host.cmd(f'python3 -c "{client_script}"')
+        print("Output del client:")
+        print(output)
 
 
